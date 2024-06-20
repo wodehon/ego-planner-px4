aa2058f4ddf53286aa327386e0e94f1891c3df94
wodehon <3160103979@zju.edu.cn>
2024-06-20 19:27:20 +0800

    change ros source file, use 'source src/load_px4_gazebo.sh' to source rospath

--- src/load_px4_gazebo.sh
+++ src/load_px4_gazebo.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # source /opt/ros/melodic/setup.zsh
 # px4
-source ~/catkin_ws/devel/setup.zsh   # mavros
-source ~/Code/PX4-Autopilot/Tools/setup_gazebo.bash ~/Code/PX4-Autopilot/ ~/Code/PX4-Autopilot/build/px4_sitl_rtps
+# source ~/catkin_ws/devel/setup.zsh   # mavros
+source /home/dev/code/ego-planner-px4/devel/setup.zsh --extend
+source /home/dev/code/PX4-Autopilot/dev-v1.14.0-fast_planner/Tools/simulation/gazebo-classic/setup_gazebo.bash /home/dev/code/PX4-Autopilot/dev-v1.14.0-fast_planner /home/dev/code/PX4-Autopilot/dev-v1.14.0-fast_planner/build/px4_sitl_devel
 # run next two line in terminal
-export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:~/Code/PX4-Autopilot
-export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:~/Code/PX4-Autopilot/Tools/sitl_gazebo
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/dev/code/PX4-Autopilot/dev-v1.14.0-fast_planner
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/dev/code/PX4-Autopilot/dev-v1.14.0-fast_planner/Tools/simulation/gazebo-classic/sitl_gazebo-classic
 
 
